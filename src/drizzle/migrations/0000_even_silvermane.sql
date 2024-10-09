c4f7730b528dbfa3238062bc0739ae3a57e68bb2
Tyloo <jbonva@gmail.com>
2024-10-09 18:14:55 +0200

    chore: added database migration
    Signed-off-by: Tyloo <jbonva@gmail.com>

---
+++ src/drizzle/migrations/0000_even_silvermane.sql
@@ -0,0 +1,42 @@
+DO $$ BEGIN
+ CREATE TYPE "public"."day" AS ENUM('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "event" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"clerk_user_id" uuid NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"duration_in_minutes" integer NOT NULL,
+	"is_active" boolean DEFAULT true NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "schedule_availability" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"schedule_id" uuid NOT NULL,
+	"start_time" text NOT NULL,
+	"end_time" text NOT NULL,
+	"day_of_week" "day" NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "schedule" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"clerk_user_id" text NOT NULL,
+	"timezone" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "schedule_clerk_user_id_unique" UNIQUE("clerk_user_id")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "schedule_availability" ADD CONSTRAINT "schedule_availability_schedule_id_schedule_id_fk" FOREIGN KEY ("schedule_id") REFERENCES "public"."schedule"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "clerk_user_id_index" ON "event" USING btree ("clerk_user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "schedule_id_index" ON "schedule_availability" USING btree ("schedule_id");
