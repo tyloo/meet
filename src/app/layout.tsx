42da8338c74c5ab72c3cdd54f68bbb4b5e707e6a
Tyloo <jbonva@gmail.com>
2024-10-09 16:17:33 +0200

    chore: added chadcn
    Signed-off-by: Tyloo <jbonva@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`min-h-screen bg-background ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
