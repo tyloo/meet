2fa00759b9f4d7c0a3567fdd0bd2bf16340f1139
Tyloo <jbonva@gmail.com>
2024-10-09 17:41:13 +0200

    chore: added middleware and drizzle
    Signed-off-by: Tyloo <jbonva@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
 export default function HomePage() {
-  return null
+  const { userId } = auth()
+  if (userId !== null) {
+    redirect('/events')
+  }
+
+  return (
+    <div className="text-center container my-4 mx-auto">
+      <h1 className="text-3xl mb-4">Fancy Home Page</h1>
+      <div className="flex gap-2 justify-center">
+        <Button asChild><SignInButton /></Button>
+        <Button asChild><SignUpButton /></Button>
+        <UserButton />
+      </div>
+    </div>
+  )
 }
---
+++ src/drizzle/db.ts
@@ -0,0 +1,7 @@
+import process from 'node:process'
+import { neon } from '@neondatabase/serverless'
+
+import { drizzle } from 'drizzle-orm/neon-http'
+
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle(sql)
