23fa7717647db55e1892934f660f632c1a8a742e
Tyloo <jbonva@gmail.com>
2024-10-10 14:46:40 +0200

    chore: added migration and navbar
    Signed-off-by: Tyloo <jbonva@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,10 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   react: true,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
   ignores: [
     '**/components/ui',
   ],
