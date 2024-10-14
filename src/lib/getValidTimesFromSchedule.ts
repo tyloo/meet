2e5c18b21fca36d94aeb143355f9f0dc0effdcd4
Tyloo <jbonva@gmail.com>
2024-10-14 15:48:19 +0200

    chore: added sync to Google Calendar
    Signed-off-by: Tyloo <jbonva@gmail.com>

---
+++ src/lib/getValidTimesFromSchedule.ts
@@ -0,0 +1,139 @@
+import { DAYS_OF_WEEK_IN_ORDER } from "@/data/constants";
+import { db } from "@/drizzle/db";
+import { ScheduleAvailabilityTable } from "@/drizzle/schema";
+import { getCalendarEventTimes } from "@/server/googleCalendar";
+import {
+  addMinutes,
+  areIntervalsOverlapping,
+  isFriday,
+  isMonday,
+  isSaturday,
+  isSunday,
+  isThursday,
+  isTuesday,
+  isWednesday,
+  isWithinInterval,
+  setHours,
+  setMinutes,
+} from "date-fns";
+import { fromZonedTime } from "date-fns-tz";
+
+export async function getValidTimesFromSchedule(
+  timesInOrder: Date[],
+  event: { clerkUserId: string; durationInMinutes: number }
+) {
+  const start = timesInOrder[0];
+  const end = timesInOrder.at(-1);
+
+  if (start == null || end == null) {
+    return [];
+  }
+
+  const schedule = await db.query.ScheduleTable.findFirst({
+    where: ({ clerkUserId: userIdCol }, { eq }) =>
+      eq(userIdCol, event.clerkUserId),
+    with: { availabilities: true },
+  });
+
+  if (schedule == null) {
+    return [];
+  }
+
+  const groupedAvailabilities = Object.groupBy(
+    schedule.availabilities,
+    (a) => a.dayOfWeek
+  );
+
+  const eventTimes = await getCalendarEventTimes(event.clerkUserId, {
+    start,
+    end,
+  });
+
+  return timesInOrder.filter((intervalDate) => {
+    const availabilities = getAvailabilities(
+      groupedAvailabilities,
+      intervalDate,
+      schedule.timezone
+    );
+    const eventInterval = {
+      start: intervalDate,
+      end: addMinutes(intervalDate, event.durationInMinutes),
+    };
+
+    return (
+      eventTimes.every((eventTime) => {
+        return !areIntervalsOverlapping(eventTime, eventInterval);
+      }) &&
+      availabilities.some((availability) => {
+        return isWithinInterval(eventInterval.start, availability!);
+      })
+    );
+  });
+}
+
+function getAvailabilities(
+  groupedAvailabilities: Partial<
+    Record<
+      (typeof DAYS_OF_WEEK_IN_ORDER)[number],
+      (typeof ScheduleAvailabilityTable.$inferSelect)[]
+    >
+  >,
+  date: Date,
+  timezone: string
+) {
+  let availabilities:
+    | (typeof ScheduleAvailabilityTable.$inferSelect)[]
+    | undefined;
+
+  if (isMonday(date)) {
+    availabilities = groupedAvailabilities.monday;
+  }
+  if (isTuesday(date)) {
+    availabilities = groupedAvailabilities.tuesday;
+  }
+  if (isWednesday(date)) {
+    availabilities = groupedAvailabilities.wednesday;
+  }
+  if (isThursday(date)) {
+    availabilities = groupedAvailabilities.thursday;
+  }
+  if (isFriday(date)) {
+    availabilities = groupedAvailabilities.friday;
+  }
+  if (isSaturday(date)) {
+    availabilities = groupedAvailabilities.saturday;
+  }
+  if (isSunday(date)) {
+    availabilities = groupedAvailabilities.monday;
+  }
+
+  if (availabilities == null) {
+    return [];
+  }
+
+  return availabilities.map(({ startTime, endTime }) => {
+    if (!startTime || !endTime) {
+      return null;
+    }
+    const [startHour, startMimute] = startTime.split(":") || [];
+    if (!startHour || !startMimute) {
+      return null;
+    }
+    const [endHour, endMimute] = endTime.split(":") || [];
+    if (!endHour || !endMimute) {
+      return null;
+    }
+
+    const start = fromZonedTime(
+      setMinutes(setHours(date, parseInt(startHour)), parseInt(startMimute)),
+      timezone
+    );
+
+    const end = fromZonedTime(
+      setMinutes(setHours(date, parseInt(endHour)), parseInt(endMimute)),
+      timezone
+    );
+
+    return { start, end };
+  });
+}
---
+++ src/server/googleCalendar.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import "use-server";
+import { clerkClient } from "@clerk/nextjs/server";
+import { google } from "googleapis";
+import { endOfDay, startOfDay } from "date-fns";
+
+export async function getCalendarEventTimes(
+  clerkUserId: string,
+  { start, end }: { start: Date; end: Date }
+) {
+  const oAuthClient = await getOAuthClient(clerkUserId);
+
+  const events = await google.calendar("v3").events.list({
+    calendarId: "primary",
+    eventTypes: ["default"],
+    singleEvents: true,
+    timeMin: start.toISOString(),
+    timeMax: end.toISOString(),
+    maxResults: 2500,
+    auth: oAuthClient,
+  });
+
+  return (
+    events.data.items
+      ?.map((event) => {
+        if (event.start?.date != null && event.end?.date != null) {
+          return {
+            start: startOfDay(event.start.date),
+            end: endOfDay(event.end.date),
+          };
+        }
+
+        if (event.start?.dateTime != null && event.end?.dateTime != null) {
+          return {
+            start: new Date(event.start.dateTime),
+            end: new Date(event.end.dateTime),
+          };
+        }
+      })
+      .filter((date) => date != null) || []
+  );
+}
+
+async function getOAuthClient(clerkUserId: string) {
+  const token = await clerkClient().users.getUserOauthAccessToken(
+    clerkUserId,
+    "oauth_google"
+  );
+  if (token.data.length === 0 || token.data[0]?.token == null) {
+    return;
+  }
+
+  const client = new google.auth.OAuth2(
+    process.env.GOOGLE_OAUTH_CLIENT_ID,
+    process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+    process.env.GOOGLE_OAUTH_REDIRECT_URL
+  );
+  client.setCredentials({ access_token: token.data[0].token });
+
+  return client;
+}
